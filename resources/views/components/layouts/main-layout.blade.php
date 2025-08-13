b8b7e96e574d38747d5a8080ff746c18cfda87e6
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-08-13 12:46:03 -0300

    explicando componentes rotas e o middleware

--- resources/views/components/layouts/main-layout.blade.php
+++ resources/views/components/layouts/main-layout.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>[page_title]</title>
+    <title>{{ $pageTitle }}</title>
     <link rel="stylesheet" href="{{ asset('assets/bootstrap/bootstrap.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/fontawesome/css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('assets/css/main.css') }}">
