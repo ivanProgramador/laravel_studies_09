9b9c0cf2fd585fe193d0b01b9a86cadebdfd1d97
Ivan Rodrigues <ivan-guita@hotmail.com>
2025-08-12 21:53:35 -0300

    migrations e seeds criados para registrar os usuarios em loop

---
+++ database/seeders/UsersTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Carbon;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+         for($index = 1;$index <= 3; $index++){
+         
+         User::create([
+             'user_name'=>"user$index",
+             'email'=>"user$index@gmail.com",
+             'password'=>bcrypt('Aa123456'),
+             'email_verified_at'=>Carbon::now(),
+             'active'=>true
+            ]);
+    }
+}
+}
